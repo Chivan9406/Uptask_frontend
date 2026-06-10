c8cd110820d85b629b466a599b0852a7ace4ba5a
Chivan9406 <ivan9406@hotmail.com>
2026-06-09 21:44:07 -0600

    feat: add routing with react-router, create layout and views for the application

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from 'react-router'
+import Logo from '../components/Logo'
+
+export default function AppLayout() {
+  return (
+    <>
+      <header className="bg-gray-800 py-5">
+        <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-between">
+          <div className="w-64">
+            <Logo />
+          </div>
+        </div>
+      </header>
+
+      <section className="max-w-7xl mx-auto mt-10 p-5">
+        <Outlet />
+      </section>
+
+      <footer className="py-5">
+        <p className="text-center">Todos los derechos reservados {new Date().getFullYear()}</p>
+      </footer>
+    </>
+  )
+}
