97a18c5057842bf9c4b9662c7481cf9747ca8541
Chivan9406 <ivan9406@hotmail.com>
2026-06-10 19:56:14 -0600

    feat: add CreateProjectView component and update routing in the application

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,17 +1,17 @@
 import { Link } from 'react-router'
 
-export default function DashboardView() {
+export default function CreateProjectView() {
   return (
     <>
-      <h1 className="text-5xl font-black">Mis proyectos</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
+      <h1 className="text-5xl font-black">Crear proyecto</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">Llena el siguiente formulario para crear un proyecto</p>
 
       <nav className="my-5">
         <Link
-          to="/projects/create"
+          to="/"
           className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
         >
-          Nuevo proyecto
+          Volver a proyectos
         </Link>
       </nav>
     </>
