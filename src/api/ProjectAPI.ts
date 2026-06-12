e23aebf6456e3accec852b36f3ad18c164f78013
Chivan9406 <ivan9406@hotmail.com>
2026-06-12 12:43:39 -0600

    feat: integrate react-query for project creation with error handling and success notifications

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,11 +1,14 @@
 import type { ProjectFormData } from '@/types/index'
 import api from '@/lib/axios'
+import { isAxiosError } from 'axios'
 
 export async function createProject(formData: ProjectFormData) {
   try {
     const { data } = await api.post('/projects', formData)
     return data
-  } catch (e) {
-    console.error(e)
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error, { cause: error })
+    }
   }
 }
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -4,6 +4,7 @@ import ProjectForm from '@/components/projects/ProjectForm'
 import type { ProjectFormData } from '@/types/index'
 import { createProject } from '@/api/ProjectAPI'
 import { toast } from 'react-toastify'
+import { useMutation } from '@tanstack/react-query'
 
 export default function CreateProjectView() {
   const navigate = useNavigate()
@@ -20,11 +21,18 @@ export default function CreateProjectView() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues })
 
-  const handleForm = async (formData: ProjectFormData) => {
-    const data = await createProject(formData)
-    toast.success(data)
-    navigate('/')
-  }
+  const { mutate } = useMutation({
+    mutationFn: createProject,
+    onError: error => {
+      toast.error(error.message)
+    },
+    onSuccess: data => {
+      toast.success(data)
+      navigate('/')
+    },
+  })
+
+  const handleForm = (formData: ProjectFormData) => mutate(formData)
 
   return (
     <>
