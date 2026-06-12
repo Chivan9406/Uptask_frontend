8ea4b4bc8b5ca6e72f0eec0e167ad5a3e4dc4e46
Chivan9406 <ivan9406@hotmail.com>
2026-06-12 17:26:25 -0600

    feat: implement project dashboard with data fetching and conditional rendering

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,19 +1,104 @@
 import { Link } from 'react-router'
+import { useQuery } from '@tanstack/react-query'
+import { getProjects } from '@/api/ProjectAPI.ts'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
 
 export default function DashboardView() {
-  return (
-    <>
-      <h1 className="text-5xl font-black">Mis proyectos</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ['projects'],
+    queryFn: getProjects,
+  })
 
-      <nav className="my-5">
-        <Link
-          to="/projects/create"
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-        >
-          Nuevo proyecto
-        </Link>
-      </nav>
-    </>
-  )
+  if (isLoading) return 'Cargando...'
+
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">Mis proyectos</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
+
+        <nav className="my-5">
+          <Link
+            to="/projects/create"
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          >
+            Nuevo proyecto
+          </Link>
+        </nav>
+
+        {data.length ? (
+          <ul
+            role="list"
+            className="divide-y divide-gray-100 border border-gray-100 mt-10 bg-white shadow-lg"
+          >
+            {data.map(project => (
+              <li key={project._id} className="flex justify-between gap-x-6 px-5 py-10">
+                <div className="flex min-w-0 gap-x-4">
+                  <div className="min-w-0 flex-auto space-y-2">
+                    <Link
+                      to={``}
+                      className="text-gray-600 cursor-pointer hover:underline text-3xl font-bold"
+                    >
+                      {project.projectName}
+                    </Link>
+                    <p className="text-sm text-gray-400 my-2">Cliente: {project.clientName}</p>
+                    <p className="text-sm text-gray-400 my-2">{project.description}</p>
+                  </div>
+                </div>
+                <div className="flex shrink-0 items-center gap-x-6">
+                  <Menu as="div" className="relative">
+                    <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900 transition-colors cursor-pointer">
+                      <span className="sr-only">opciones</span>
+                      <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
+                    </MenuButton>
+
+                    <MenuItems
+                      transition
+                      className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none transition duration-100 ease-in-out transform opacity-100 scale-100 data-closed:opacity-0 data-closed:scale-95"
+                    >
+                      <MenuItem>
+                        <Link
+                          to={``}
+                          className="block px-3 py-1 text-sm leading-6 text-gray-900 hover:text-gray-950 transition-colors"
+                        >
+                          Ver Proyecto
+                        </Link>
+                      </MenuItem>
+                      <MenuItem>
+                        <Link
+                          to={``}
+                          className="block px-3 py-1 text-sm leading-6 text-gray-900 hover:text-gray-950 transition ease-in duration-75"
+                        >
+                          Editar Proyecto
+                        </Link>
+                      </MenuItem>
+                      <MenuItem>
+                        <button
+                          type="button"
+                          className="block px-3 py-1 text-sm leading-6 text-red-500 hover:text-red-600 transition-colors w-full cursor-pointer text-left"
+                          onClick={() => {}}
+                        >
+                          Eliminar Proyecto
+                        </button>
+                      </MenuItem>
+                    </MenuItems>
+                  </Menu>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-center py-20">
+            Aún no hay proyectos {''}
+            <Link
+              to="/projects/create"
+              className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            >
+              Crear proyecto
+            </Link>
+          </p>
+        )}
+      </>
+    )
 }
